b54776db9e66157be38dba4d55f9c4666b7c6e91
roismohammed <id.roismohammed@gmail.com>
2024-07-06 15:35:46 +0700

    df

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react'
-import Guest from './layouts/Guest'
-import { Typewriter } from 'react-simple-typewriter'
-import { RiArrowUpFill, RiFacebookFill, RiInstagramFill, RiTiktokFill, RiYoutubeFill } from "react-icons/ri";
-import './index.css'
-import { motion } from "framer-motion"
+import React, { useState } from 'react';
+import Guest from './layouts/Guest';
+import { Typewriter } from 'react-simple-typewriter';
+import { RiArrowDownFill, RiArrowUpFill, RiFacebookFill, RiInstagramFill, RiTiktokFill, RiYoutubeFill } from "react-icons/ri";
+import './index.css';
+import { motion } from "framer-motion";
 import ButtonGlow from './components/ButtonGlow';
+
 export default function Index() {
-  const [stikyIcon, setStikyIcon] = useState(false)
+  const [stikyIcon, setStikyIcon] = useState(false);
 
   const changeStiky = () => {
     if (window.scrollY >= 50) {
-      setStikyIcon(true)
+      setStikyIcon(true);
     } else {
-      setStikyIcon(false)
+      setStikyIcon(false);
     }
-  }
+  };
 
   window.addEventListener('scroll', () => {
-    changeStiky()
-  })
+    changeStiky();
+  });
+
   return (
     <Guest>
       <div id='atas' style={{ backgroundColor: '#2B2A2B', minHeight: '100vh' }}>
-        <div className="container" >
+        <div className="container">
           <motion.div
             variants={{
               hidden: { opacity: 0, y: -35 },
@@ -30,10 +32,10 @@ export default function Index() {
             }} initial="hidden"
             animate="visible"
             transition={{ duration: 0.5, delay: 0.25 }}>
-            <div className="row  d-flex mt-4 align-items-center vh-100 mx-md-4 isi-index ">
-              <div className="col-md-7 p-3 ">
-                <div className="text-white ">
-                  <h1 className='text1 '>Hello it's me
+            <div className="row d-flex mt-4 align-items-center vh-100 mx-md-4 isi-index">
+              <div className="col-md-7 p-3">
+                <div className="text-white">
+                  <h1 className='text1'>Hello it's me
                     <span className='tangan'>👋🏻</span></h1>
                   <h1 className='text2 d-flex'> Muhammad_ <span> Rois</span></h1>
                   <h1 className='and'> And i'm a
@@ -46,45 +48,32 @@ export default function Index() {
                         deleteSpeed={50}
                         delaySpeed={1000}
                         words={['Front-end Dev', 'Programmer', 'Photografer']}
-
                       />
                     </span>
                   </h1>
-                  <p className='fs-2 text3'> "Saya seorang Software Engineer junior dari Indonesia yang menggunakan JavaScript dan Framework React.js" Saat ini, saya bekerja sebagai freelancer"</p>
-                  {/* icon */}
+                  <p className='fs-2 text3'>"Saya seorang Software Engineer junior dari Indonesia yang menggunakan JavaScript dan Framework React.js" Saat ini, saya bekerja sebagai freelancer"</p>
                   <div className='d-flex gap-4 mt-3 mx-2 icon-index'>
                     <div className="menu-icon">
-                      <a
-                        href="https://web.facebook.com/uhammadrois"
-                        className="index-icon">
+                      <a href="https://web.facebook.com/uhammadrois" className="index-icon">
                         <RiFacebookFill />
                       </a>
                     </div>
-
-                    <div className="menu-icon ">
-                      <a
-                        href="https://www.instagram.com/roess___/"
-                        className="index-icon">
+                    <div className="menu-icon">
+                      <a href="https://www.instagram.com/roess___/" className="index-icon">
                         <RiInstagramFill />
                       </a>
                     </div>
-
-                    <div className="menu-icon ">
-                      <a
-                        href="https://www.tiktok.com/@roisdev_?_t=8jhHW15U60m&_r=1"
-                        className="index-icon">
+                    <div className="menu-icon">
+                      <a href="https://www.tiktok.com/@roisdev_?_t=8jhHW15U60m&_r=1" className="index-icon">
                         <RiTiktokFill />
                       </a>
                     </div>
-
-                    <div className="menu-icon ">
+                    <div className="menu-icon">
                       <a href='https://www.youtube.com/channel/UCN7HkTZtjILbDCQzzzHCeWg' className="index-icon">
                         <RiYoutubeFill />
                       </a>
                     </div>
                   </div>
-
-                  {/* tombol */}
                   <div className='mt-3'>
                     <a href="../rois.pdf">
                       <ButtonGlow nama={'DOWNLOAD CV'} />
@@ -92,19 +81,24 @@ export default function Index() {
                   </div>
                 </div>
               </div>
-
               <div className="col-md-5 bg-foto-index">
                 <img src="../indexfoto.png" className='foto-index d-none d-md-block' alt="" />
               </div>
             </div>
           </motion.div>
         </div>
-        {/* icon  */}
-        <div className={stikyIcon ? 'hidden text-white text-end fixed-bottom p-5 ' : 'text-white text-end fixed-bottom p-5 stiky-icon'}>
-          <a href="#atas" >
-            <RiArrowUpFill className='icon-atas' /></a>
+        
+        <div className={stikyIcon ? 'hidden text-white text-end fixed-bottom p-5' : 'text-white text-end fixed-bottom p-5 stiky-icon'}>
+          <a href="#atas">
+            <RiArrowUpFill className='icon-atas' />
+          </a>
+        </div>
+
+        <div className="d-flex text-scroll flex-column align-items-center text-white">
+          <h4>Scroll Down For More <RiArrowDownFill size={25}/></h4>
+          
         </div>
       </div>
-    </Guest >
-  )
+    </Guest>
+  );
 }
